3f110e231d21a3ff553c264230afea1a4d4afbee
quangtd-kozocom <quangtd@kozo-japan.com>
2026-06-12 09:16:57 +0700

    Remove unnessary test.spec.ts

--- tests/gen/HAPPYS/test.spec.ts
+++ tests/gen/HAPPYS/test.spec.ts
@@ -2,17 +2,4 @@ import { test, expect } from "@playwright/test";
 
 // <bull-terra:manual>
 
-test("TC-31: 3131", async ({ page }) => {
-  await page.goto('http://happys.local/staff/1995156432/points');
-    await page.locator('#nav-staff_18').getByRole('link', { name: '新規登録' }).click();
-    await page.locator('#point_master_id').selectOption('18');
-    page.once('dialog', dialog => {
-      console.log(`Dialog message: ${dialog.message()}`);
-      dialog.dismiss().catch(() => {});
-    });
-    await page.locator('#submit_btn').click();
-  // TODO: replace this default smoke assertion with one specific to this test case.
-  await expect.soft(page.locator("body"), "default smoke assertion").toBeAttached();
-});
-
 // </bull-terra:manual>
